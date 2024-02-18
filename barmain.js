f69047287a6917034d5b36083dbeebe06ea1f143
Jiatong Sun <127688234+jiatongs@users.noreply.github.com>
2024-02-17 23:07:03 -0500

    Add files via upload

--- barmain.js
+++ barmain.js
@@ -81,7 +81,28 @@ tabBtns.forEach((tabBtn, i) => {
     });
 });
 
+//Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".porfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
 
+var portfolioModal = function (modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
 
 
 
